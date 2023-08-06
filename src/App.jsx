9cf5c042fcfa3da6428b7c9bc2e2554f0ac89c69
Valentin Tur <valentin.rj.tur@gmail.com>
2023-08-06 19:02:08 -0300

    Muchas modificaciones y trabajo en el apartado de productos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting = {'¡Bienvenidos!'}/>
+      <ItemListContainer />
     </>
     
   )
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,48 @@
 import './ItemListContainer.css'
+import data from "../../data/productos.json"
+import { useEffect, useState } from 'react'
 
-export const ItemListContainer = ({greeting}) => {
 
-    return <h1 className="titulo"> {greeting} </h1>
+export const ItemListContainer = () => {
 
-}
+    const [productos, setProductos] = useState([]);
+
+    const pedirProductos = () => {
+        return new Promise((resolve, reject) => {
+            resolve(data)
+        })
+    }
+
+    useEffect( () => {
+        pedirProductos() 
+        .then((res) => {
+            setProductos(res);     
+        })
+    }, [])
+
+    return ( 
+        <div className='conteiner_products'>
+        <h1 className='titulo'>Productos</h1>
+        <div className="productos-container"> 
+          {productos.length > 0 &&
+            productos.map((producto) => {
+              return (
+                <div key={producto.id} className="card"> 
+                <img className='img' src={`/img/${producto.img}`} alt={producto.nombre} />
+                  <div className="card-body">
+                    <h5 className="card-title">{producto.titulo}</h5>
+                    <p className="card-text">Precio: ${producto.precio}.</p>
+                    <a href="#" className="btn btn-primary button">Comprar <i className="bi bi-cart carrito"></i></a>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+      </div>
+    );
+  
+        }
+
+
+
+
